5a452dfef3893309946a26a6c7b37726ac567610
Artem <93729278+Nikolaev3Artem@users.noreply.github.com>
2022-11-17 12:00:45 +0200

    added crawling

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,68 +1,119 @@
-using System.Collections;
 using UnityEngine;
-
-public class Player
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
 {
-    private int _health;
-    
-    public int Health
+    [SerializeField] private float moveForce = 10f;
+    [SerializeField] private float jumpForce = 11f;
+    private float movementX;
+    private float movementY;
+    private bool crawl = false;
+
+    private Rigidbody2D body;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    //Animations states
+    private string WALK_UP_ANIMATION = "Walk_Up";
+    private string WALK_DOWN_ANIMATION = "Walk_Down";
+    private string WALK_SIDE_ANIMATION = "Walk_Side";
+    private string CRAWL_UP_ANIMATION = "Crawl_Up";
+    private string CRAWL_DOWN_ANIMATION = "Crawl_Down";
+    private string CRAWL_SIDE_ANIMATION = "Crawl_Side";
+
+    private void Awake()
     {
-        get
-        {
-            return _health;
-        }
-        set
-        {
-            _health = value;
-        }
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    private int _damage;
-
-    public int Damage
+    private void Update()
     {
-        get
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+        movementY = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(0f, movementY, 0f) * Time.deltaTime * moveForce;
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
         {
-            return _damage;
+            crawl = true;
         }
-        set
+        else
         {
-            _damage = value;
+            crawl = false;
         }
+
     }
 
-    private string _name;
-
-    public string Name
+    void AnimatePlayer()
     {
-        get
+        if (movementY > 0)
         {
-            return _name;
+            if (crawl != false)
+            {
+                anim.SetBool(CRAWL_UP_ANIMATION, true);
+            }
+            else
+            {
+                anim.SetBool(WALK_UP_ANIMATION, true);
+            }
+
         }
-        set
+        else if (movementY < 0)
         {
-            _name = value;
+            if (crawl != false)
+            {
+                anim.SetBool(CRAWL_DOWN_ANIMATION, true); 
+            }
+            else
+            {
+                anim.SetBool(WALK_DOWN_ANIMATION, true);
+            }
+
+    }
+        else
+        {
+            anim.SetBool(WALK_UP_ANIMATION, false);
+            anim.SetBool(WALK_DOWN_ANIMATION, false);
+            anim.SetBool(CRAWL_UP_ANIMATION, false);
+            anim.SetBool(CRAWL_DOWN_ANIMATION, false);
+
+        }
+
+        if (movementX > 0)
+        {
+            if(crawl != false)
+            {
+                anim.SetBool(CRAWL_SIDE_ANIMATION, true);
+            }
+            else
+            {
+                anim.SetBool(CRAWL_SIDE_ANIMATION, false);
+                anim.SetBool(WALK_SIDE_ANIMATION, true);
+            }
+            sr.flipX = true;
+        }
+        else if(movementX < 0)
+        {
+            if (crawl != false)
+            {
+                anim.SetBool(CRAWL_SIDE_ANIMATION, true);
+            }
+            else
+            {
+               anim.SetBool(WALK_SIDE_ANIMATION, true);
+            }
+            sr.flipX = false;
+        }
+        else
+        {
+            anim.SetBool(CRAWL_SIDE_ANIMATION, false);
+            anim.SetBool(WALK_SIDE_ANIMATION, false);
         }
     }
-
-    public Player() { }
-
-    public Player(int health, int damage, string name)
-    {
-        Health = health;
-        Damage = damage;
-        Name = name;
-    }
-
-    public virtual void Attack()
-    {
-        Debug.Log(Name + " is shooting.");
-    }
-
-    public void Info()
-    {
-        Debug.Log("Health is: " + Health + ", Damage is: " + Damage + ", Name is: " + Name);
-    }
-
-
-} // class
+}
