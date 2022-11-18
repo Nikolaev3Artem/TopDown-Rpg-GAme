f96b7de1b65e317d81ed6bf22345756c0c5a89b6
Artem <93729278+Nikolaev3Artem@users.noreply.github.com>
2022-11-18 20:59:35 +0200

    added chazing behaviour

--- Assets/Scripts/Fox.cs
+++ Assets/Scripts/Fox.cs
@@ -1,27 +1,19 @@
 using UnityEngine;
-
-
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Animator))]
+using UnityEngine.AI;
 public class Fox : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-    public GameObject target;
-
-    private Rigidbody2D body;
-    private Animator anim;
-
-    private string RUN_ANIMATION = "Run";
-
-    private void Awake()
+    [SerializeField] Transform target;
+    private NavMeshAgent agent;
+    private void Start()
     {
-        body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     private void Update()
     {
-        body.transform.position = new Vector3(body.transform.position.x, body.transform.position.y, 0) * speed * Time.deltaTime;
-        anim.SetBool(RUN_ANIMATION, true);
+        agent.SetDestination(target.position);
     }
 }
